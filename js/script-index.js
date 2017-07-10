529fe6933a21d1bf74c6a3a5c00457b43b037b35
kathmarioly <katerinemarioly@gmail.com>
2017-07-10 11:15:51 -0400

    Icono FLECHA oculta al mostrar el index.html, icono MENU oculto al mostrar recipe.html

--- js/script-index.js
+++ js/script-index.js
@@ -1,5 +1,5 @@
 $(document).ready( function(){
-
+	$(".js-back").hide();
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 	renderHighlightedRecipes(recipesArray);
 
@@ -41,5 +41,4 @@ function renderActivities(activitiesArray) {
 function renderActivity(recipe) {
 	
 }
-
 
